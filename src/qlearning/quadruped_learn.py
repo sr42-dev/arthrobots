80806fc2c255407d82dada3046afe8734f8d4add
Sriram Radhakrishna <sriram.radhakrishna42@gmail.com>
2023-11-04 16:07:57 +0530

    Saving progress, modified worldmodels file structure (unrelated) on qlearning_devel
    next immediate items (  to test after each item) :
    - implement q learning on single arthrobot simulation
    - implement dreamerv3 on single arthrobot simulation
    - plot rewards vs episodes for all methods
    - implement model saving and deployment for all methods
    - build and test hardware with dreamerv3 (off-repo)
    - allocate gpu resources
    - federate best model
    
    long term milestones (publish to main after each item) :
    - survey of walking RL algorithms <
    - dreamerv3 on arthrobot
    - walking to different goals using discrete mapping
    - survey on federation methods for dreamer
    - best type of federation in LFRL for legged robots
    - improving path planning using RL (FRedBots inspired)
    - improving scheduling from alica and optimized with RL
    - dynamic multi-agent obstacle avoidance methods (when agents become obstacles for one another)
    - Arthrobots : Adaptive swarms

--- src/qlearning/quadruped_learn.py
+++ src/qlearning/quadruped_learn.py
@@ -1,61 +1,128 @@
 #/usr/bin/env python3
-from __future__ import print_function
+from qlearning import QL, ReplayBuffer
 from quadruped_env import QuadrupedEnvironment
-import numpy as np
+import numpy 
+import time
 
-env = QuadrupedEnvironment()    #functions - init, jsp_callback, normalize_js, imu_sub_callback
-                                # reset, step, 
-state_shape = env.state_shape[0] #this returns tuple (34,) so we need to get only the first int
-action_shape = env.action_shape[0] #this needs to be indexed too; same case as above
-agent = #add your algorithm file class here
+env = QuadrupedEnvironment()
+last_time_steps = numpy.ndarray(0)
+qlearn = QL(actions=range(env.action_shape[0]),alpha=0.2, gamma=0.8, epsilon=0.9)
 
-print('A2C agent configured') 
-max_episode = 10000
-tot_rewards = []
-print('env reset')
-print("\n*************************")
+initial_epsilon = qlearn.epsilon
+epsilon_discount = 0.9986
+start_time = time.time()
+total_episodes = 10000
+highest_reward = 0
 
-observation, done = env.reset() #gazebo reset + states are 0 + join states are set to default
-print("\n**********************")
-print("obs, done? ",observation, done)
-print("\n***********************")
-action = agent.select_action(observation) # a random int action is taken, with prob = action_probs
-print("Action after select_action(): ",action)
-print("\n*********************")
-observation, reward, done = env.step(action)
+for x in range(total_episodes):
 
-noise_sigma = 0.1
-save_cutoff = 1
-cutoff_count = 0
-save_count = 0
+    done = False
+    cumulated_reward = 0
+    observation, _ = env.reset()
+    if qlearn.epsilon > 0.05:
+        qlearn.epsilon *= epsilon_discount
+    state = ''.join(map(str, observation))
 
-curr_highest_eps_reward = -1000.0
-for i in range(max_episode):
-    if i % 100 == 0 and noise_sigma>0.03:
-        agent.noise = 0.05 #constant noise for now 
-        noise_sigma /= 2.0
-    step_num = 0
-    while done == False:
-        step_num += 1
-        state_val = env.step(action)[0]
-        action_final = agent.select_action(state_val)
-        print("Action after reset ",action_final)
-        print("\n*********************")
-        observation, reward, done = env.step(action_final[0])
-        print('reward:',reward,'episode:', i, 'step:',step_num,'curr high eps reward:',curr_highest_eps_reward, 'saved:',save_count, 'cutoff count:', cutoff_count)
-    action, eps_reward = env.step(action)
-    tot_rewards.append(eps_reward)
-    if eps_reward > curr_highest_eps_reward:
-        cutoff_count += 1
-        curr_highest_eps_reward = eps_reward
-    if cutoff_count >= save_cutoff:
-        save_count += 1
-        print('saving_model at episode:',i)
-        agent.save_model()
-        agent.save_memory()
-        cutoff_count = 0
-    observation, done = env.reset()
-np.save('eps_rewards',tot_rewards)
+    for i in range(1500):
+        # Pick an action based on the current state
+        action = qlearn.chooseAction(state)
 
-import matplotlib.pyplot as plt
-plt.plot(tot_rewards)
+        # Execute the action and get feedback
+        observation, reward, done, info = env.step(action)
+        cumulated_reward += reward
+
+        if highest_reward < cumulated_reward:
+            highest_reward = cumulated_reward
+
+        nextState = ''.join(map(str, observation))
+
+        qlearn.learn(state, action, reward, nextState)
+        if not(done):
+            state = nextState
+        else:
+            last_time_steps = numpy.append(last_time_steps, [int(i + 1)])
+            break 
+
+    m, s = divmod(int(time.time() - start_time), 60)
+    h, m = divmod(m, 60)
+    print("EP: "+str(x+1)+" - [alpha: "+str(round(qlearn.alpha,2))+" - gamma: "+str(round(qlearn.gamma,2))+" - epsilon: "+str(round(qlearn.epsilon,2))+"] - Reward: "+str(cumulated_reward)+"     Time: %d:%02d:%02d" % (h, m, s))
+
+
+
+'''
+def evaluate_training_result(env, agent):
+    """
+    Evaluates the performance of the current DQN agent by using it to play a
+    few episodes of the game and then calculates the average reward it gets.
+    The higher the average reward is the better the DQN agent performs.
+
+    :param env: the game environment
+    :param agent: the DQN agent
+    :return: average reward across episodes
+    """
+    total_reward = 0.0
+    episodes_to_play = 10
+    for i in range(episodes_to_play):
+        state = env.reset()
+        done = False
+        episode_reward = 0.0
+        while not done:
+            action = agent.policy(state)
+            next_state, reward, done, _ = env.step(action)
+            episode_reward += reward
+            state = next_state
+        total_reward += episode_reward
+    average_reward = total_reward / episodes_to_play
+    return average_reward
+
+
+def collect_gameplay_experiences(env, agent, buffer):
+    """
+    Collects gameplay experiences by playing env with the instructions
+    produced by agent and stores the gameplay experiences in buffer.
+
+    :param env: the game environment
+    :param agent: the DQN agent
+    :param buffer: the replay buffer
+    :return: None
+    """
+    state = env.reset()
+    done = False
+    while not done:
+        action = agent.collect_policy(state)
+        next_state, reward, done = env.step(action)
+        if done:
+            reward = -1.0
+        buffer.store_gameplay_experience(state, next_state, reward, action, done)
+        state = next_state
+
+
+def train_model(max_episodes=50000):
+    """
+    Trains a DQN agent to play the quadruped game by trial and error
+
+    :return: None
+    """
+    buffer = ReplayBuffer()
+    env = QuadrupedEnvironment()
+    
+    state_shape = env.state_shape
+    action_shape = env.action_shape
+
+    agent = QL(state_shape, action_shape)
+
+    for _ in range(100):
+        collect_gameplay_experiences(env, agent, buffer)
+    for episode_cnt in range(max_episodes):
+        collect_gameplay_experiences(env, agent, buffer)
+        gameplay_experience_batch = buffer.sample_gameplay_batch()
+        loss = agent.train(gameplay_experience_batch)
+        avg_reward = evaluate_training_result(env, agent)
+        print('Episode : {0}/{1}, Reward : {2}, loss : {3}'.format(episode_cnt, max_episodes, avg_reward, loss[0]))
+        if episode_cnt % 20 == 0:
+            agent.update_target_network()
+    env.close()
+
+
+train_model()
+'''
---
+++ src/worldmodels/arthrobot-env/README.md
@@ -0,0 +1,2 @@
+# arthrobot-env
+An OpenAI Gym environment based on a Gazebo Simulation of a quadrupedal robot
