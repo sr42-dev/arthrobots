a37e29159447e212f8762239319e700520d0d6ee
Rohanmrao <rohanmaheshrao10@gmail.com>
2023-11-28 22:56:15 +0530

    qlearning model saver added

--- src/locomotion/qlearning/quadruped_learn_qlearning.py
+++ src/locomotion/qlearning/quadruped_learn_qlearning.py
@@ -3,7 +3,10 @@ from qlearning import QL, ReplayBuffer
 from quadruped_env import QuadrupedEnvironment
 import numpy 
 import time
+import tensorflow as tf
+import pickle
 
+# saver = tf.train.Saver()
 env = QuadrupedEnvironment()
 last_time_steps = numpy.ndarray(0)
 qlearn = QL(alpha=0.2, gamma=0.8, epsilon=0.9)
@@ -43,6 +46,9 @@ for x in range(total_episodes):
         else:
             last_time_steps = numpy.append(last_time_steps, [int(i + 1)])
             break 
+    
+    with open('qlearn_model.pkl', 'wb') as f:
+        pickle.dump(qlearn.q, f)
 
     m, s = divmod(int(time.time() - start_time), 60)
     h, m = divmod(m, 60)
